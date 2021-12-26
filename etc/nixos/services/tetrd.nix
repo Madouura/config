fd2b53dbf4c7e13257fb1cdd2cdda04a793a70f8
Madoura <madouura@gmail.com>
2021-12-26 07:28:57 -0600

    sdasdasdsa

--- etc/nixos/services/tetrd.nix
+++ etc/nixos/services/tetrd.nix
@@ -6,85 +6,92 @@ in {
   options.services.tetrd.enable = lib.mkEnableOption tetrd.meta.description;
 
   config = lib.mkIf config.services.tetrd.enable {
-    environment.systemPackages = [ tetrd ];
+    environment = {
+      systemPackages = [ tetrd ];
+      etc."resolv.conf".source = "/etc/tetrd/resolv.conf";
+    };
 
-    systemd.services.tetrd = {
-      description = tetrd.meta.description;
-      wantedBy = [ "multi-user.target" ];
+    systemd = {
+      tmpfiles.rules = [ "f /etc/tetrd/resolv.conf - - -" ];
 
-      serviceConfig = {
-        ExecStart = "${tetrd}/opt/Tetrd/bin/tetrd";
-        Restart = "always";
-        RuntimeDirectory = "tetrd";
-        RootDirectory = "/run/tetrd";
-        DynamicUser = true;
-        UMask = "006";
-        DeviceAllow = "usb_device";
-        LockPersonality = true;
-        MemoryDenyWriteExecute = true;
-        NoNewPrivileges = true;
-        PrivateMounts = true;
-        PrivateNetwork = lib.mkDefault false;
-        PrivateTmp = true;
-        PrivateUsers = lib.mkDefault false;
-        ProtectClock = lib.mkDefault false;
-        ProtectControlGroups = true;
-        ProtectHome = true;
-        ProtectHostname = true;
-        ProtectKernelLogs = true;
-        ProtectKernelModules = true;
-        ProtectKernelTunables = true;
-        ProtectProc = "invisible";
-        ProtectSystem = "strict";
-        RemoveIPC = true;
-        RestrictAddressFamilies = [ "AF_UNIX" "AF_INET" "AF_INET6" "AF_NETLINK" ];
-        RestrictNamespaces = true;
-        RestrictRealtime = true;
-        RestrictSUIDSGID = true;
-        SystemCallArchitectures = "native";
+      services.tetrd = {
+        description = tetrd.meta.description;
+        wantedBy = [ "multi-user.target" ];
 
-        SystemCallFilter = [
-          "@system-service"
-          "~@aio"
-          "~@chown"
-          "~@clock"
-          "~@cpu-emulation"
-          "~@debug"
-          "~@keyring"
-          "~@memlock"
-          "~@module"
-          "~@mount"
-          "~@obsolete"
-          "~@pkey"
-          "~@raw-io"
-          "~@reboot"
-          "~@swap"
-          "~@sync"
-        ];
+        serviceConfig = {
+          ExecStart = "${tetrd}/opt/Tetrd/bin/tetrd";
+          Restart = "always";
+          RuntimeDirectory = "tetrd";
+          RootDirectory = "/run/tetrd";
+          DynamicUser = true;
+          UMask = "006";
+          DeviceAllow = "usb_device";
+          LockPersonality = true;
+          MemoryDenyWriteExecute = true;
+          NoNewPrivileges = true;
+          PrivateMounts = true;
+          PrivateNetwork = lib.mkDefault false;
+          PrivateTmp = true;
+          PrivateUsers = lib.mkDefault false;
+          ProtectClock = lib.mkDefault false;
+          ProtectControlGroups = true;
+          ProtectHome = true;
+          ProtectHostname = true;
+          ProtectKernelLogs = true;
+          ProtectKernelModules = true;
+          ProtectKernelTunables = true;
+          ProtectProc = "invisible";
+          ProtectSystem = "strict";
+          RemoveIPC = true;
+          RestrictAddressFamilies = [ "AF_UNIX" "AF_INET" "AF_INET6" "AF_NETLINK" ];
+          RestrictNamespaces = true;
+          RestrictRealtime = true;
+          RestrictSUIDSGID = true;
+          SystemCallArchitectures = "native";
 
-        BindReadOnlyPaths = [
-          builtins.storeDir
-          "/etc/ssl"
-          "/etc/static/ssl"
-          "${pkgs.nettools}/bin/route:/usr/bin/route"
-          "${pkgs.nettools}/bin/ifconfig:/usr/bin/ifconfig"
-        ];
+          SystemCallFilter = [
+            "@system-service"
+            "~@aio"
+            "~@chown"
+            "~@clock"
+            "~@cpu-emulation"
+            "~@debug"
+            "~@keyring"
+            "~@memlock"
+            "~@module"
+            "~@mount"
+            "~@obsolete"
+            "~@pkey"
+            "~@raw-io"
+            "~@reboot"
+            "~@swap"
+            "~@sync"
+          ];
 
-        BindPaths = [
-          "/etc/resolv.conf"
-          "/run"
-          "/var/log"
-        ];
+          BindReadOnlyPaths = [
+            builtins.storeDir
+            "/etc/ssl"
+            "/etc/static/ssl"
+            "${pkgs.nettools}/bin/route:/usr/bin/route"
+            "${pkgs.nettools}/bin/ifconfig:/usr/bin/ifconfig"
+          ];
 
-        CapabilityBoundingSet = [
-          "CAP_DAC_OVERRIDE"
-          "CAP_NET_ADMIN"
-        ];
+          BindPaths = [
+            "/etc/tetrd/resolv.conf:/etc/resolv.conf"
+            "/run"
+            "/var/log"
+          ];
 
-        AmbientCapabilities = [
-          "CAP_DAC_OVERRIDE"
-          "CAP_NET_ADMIN"
-        ];
+          CapabilityBoundingSet = [
+            "CAP_DAC_OVERRIDE"
+            "CAP_NET_ADMIN"
+          ];
+
+          AmbientCapabilities = [
+            "CAP_DAC_OVERRIDE"
+            "CAP_NET_ADMIN"
+          ];
+        };
       };
     };
   };
