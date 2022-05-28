e0d3952aab5ba890d03e1e29cd24f89455a5fa22
Madoura <madouura@gmail.com>
2022-05-28 06:50:27 -0500

    sadasdas

--- laptop/etc/nixos/network.nix
+++ laptop/etc/nixos/network.nix
@@ -4,5 +4,11 @@
     networkmanager.enable = true;
     wireguard.enable = true;
     firewall.checkReversePath = "loose";
+
+    interfaces = {
+      enp3s0.useDHCP = true;
+      tun0.useDHCP = true;
+      wlp4s0.useDHCP = true;
+    };
   };
 }
