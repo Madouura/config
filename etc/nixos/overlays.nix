e6f531c75b7541308608f853fd26b242653acba5
Madoura <madouura@gmail.com>
2022-08-25 05:12:31 -0500

    asdasdsa

--- etc/nixos/overlays.nix
+++ etc/nixos/overlays.nix
@@ -1,11 +1,11 @@
 let
 #  nur_tar = fetchTarball "https://github.com/nix-community/nur-combined/archive/master.tar.gz";
-  vfsd_tar = fetchTarball "https://github.com/astro/nixpkgs/archive/082dd22447728db8acf13a5a013a6bff2a89f1dd.tar.gz";
+#  vfsd_tar = fetchTarball "https://github.com/astro/nixpkgs/archive/082dd22447728db8acf13a5a013a6bff2a89f1dd.tar.gz";
 in {
   nixpkgs.overlays = [
     (final: prev: {
-#      virtiofsd = (import <nixos-unstable> { }).virtiofsd;
-      virtiofsd = (import "${vfsd_tar}" { }).virtiofsd;
+      virtiofsd = (import <nixos-unstable> { }).virtiofsd;
+#      virtiofsd = (import "${vfsd_tar}" { }).virtiofsd;
     })
 
 #    (final: prev: {
